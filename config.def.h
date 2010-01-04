0fb97666ec12c10bcb8feb14bcc86a32dcdee8a9
Enno Boland (tox) <tox@s01.de>
2010-01-04 14:39:40 +0100

    dl has to be a directory, not a file.

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static char *progress_trust = "#00FF00";
 static char *stylefile      = ".surf/style.css";
 static char *scriptfile     = ".surf/script.js";
 static char *cookiefile     = ".surf/cookies.txt";
-static char *dldir          = ".surf/dl";
+static char *dldir          = ".surf/dl/";
 static time_t sessiontime   = 3600;
 
 #define SETPROP(p)       { .v = (char *[]){ "/bin/sh", "-c", \
