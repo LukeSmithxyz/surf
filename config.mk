8aabfa5b4591b020173441a7a6df631ba3ecae3f
Enno Boland (tox) <tox@s01.de>
2010-03-29 17:49:34 +0200

    whoops... linking Oo

--- config.mk
+++ config.mk
@@ -7,8 +7,8 @@ VERSION = 0.4
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-GTKINC=$(shell pkg-config --static --cflags gtk+-2.0 webkit-1.0)
-GTKLIB=$(shell pkg-config --static --libs gtk+-2.0 webkit-1.0)
+GTKINC=$(shell pkg-config --cflags gtk+-2.0 webkit-1.0)
+GTKLIB=$(shell pkg-config --libs gtk+-2.0 webkit-1.0)
 
 
 # includes and libs
@@ -18,7 +18,7 @@ LIBS = -L/usr/lib -lc ${GTKLIB} -lgthread-2.0
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\"
 CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS = -g ${LIBS} -static
+LDFLAGS = -g ${LIBS}
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
