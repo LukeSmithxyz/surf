7fcf7450925295022a94611f5d9d07694e93ec9e
Enno Boland (tox) <tox@s01.de>
2010-05-24 21:23:28 +0200

    enabling spatial-navigation

--- surf.c
+++ surf.c
@@ -508,6 +508,7 @@ newclient(void) {
 	g_object_set(G_OBJECT(settings), "auto-load-images", loadimage, NULL);
 	g_object_set(G_OBJECT(settings), "enable-plugins", plugin, NULL);
 	g_object_set(G_OBJECT(settings), "enable-scripts", script, NULL);
+	g_object_set(G_OBJECT(settings), "enable-spatial-navigation", true, NULL);
 	g_free(uri);
 
 	setatom(c, AtomFind, "");
