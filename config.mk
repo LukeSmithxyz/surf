fc3161614133b6b9e8855110859d7a96c08879d0
Connor Lane Smith <cls@lubutu.com>
2011-11-20 16:06:38 +0100

    link Xlib in config.mk

--- config.mk
+++ config.mk
@@ -7,13 +7,15 @@ VERSION = 0.4.1
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-GTKINC=$(shell pkg-config --cflags gtk+-2.0 webkit-1.0)
-GTKLIB=$(shell pkg-config --libs gtk+-2.0 webkit-1.0)
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
 
+GTKINC = `pkg-config --cflags gtk+-2.0 webkit-1.0`
+GTKLIB = `pkg-config --libs gtk+-2.0 webkit-1.0`
 
 # includes and libs
-INCS = -I. -I/usr/include ${GTKINC}
-LIBS = -L/usr/lib -lc ${GTKLIB} -lgthread-2.0
+INCS = -I. -I/usr/include -I${X11INC} ${GTKINC}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${GTKLIB} -lgthread-2.0
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\"
