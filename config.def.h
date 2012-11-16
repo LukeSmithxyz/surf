a4e1d552ca17cd3395418094fa338e097f17a7c5
Christoph Lohmann <20h@r-36.net>
2012-11-16 22:31:36 +0100

    Useragent strings do not count anymore. This is adding some text so every website (especially Google) is fooled to believe us to be compatible. The right way seems to be to add Surf at the end and let the big guys handle their useragent header war.

--- config.def.h
+++ config.def.h
@@ -1,5 +1,7 @@
 /* modifier 0 means no modifier */
-static char *useragent      = "Surf/"VERSION" (X11; U; Unix; en-US) AppleWebKit/531.2+ Compatible (Safari)";
+static char *useragent      = "Mozilla/5.0 (X11; U; Unix; en-US) "
+	"AppleWebKit/537.15 (KHTML, like Gecko) Chrome/24.0.1295.0 "
+	"Safari/537.15 Surf/"VERSION;
 static char *progress       = "#0000FF";
 static char *progress_untrust = "#FF0000";
 static char *progress_trust = "#00FF00";
