d6a3e28be24a82647ba3f0b6f8dd39d0097a4c24
Christoph Lohmann <20h@r-36.net>
2013-02-02 15:42:09 +0100

    Fixing cookie handling for https/http by using curl.

--- config.def.h
+++ config.def.h
@@ -35,10 +35,11 @@ static Bool hidebackground  = FALSE;
 /* DOWNLOAD(URI, referer) */
 #define DOWNLOAD(d, r) { \
 	.v = (char *[]){ "/bin/sh", "-c", \
-		"st -e /bin/sh -c \"wget '$0'" \
-		" --load-cookies ~/.surf/cookies.txt" \
-		" --user-agent '$1'" \
-		" --referer '$2' ; sleep 5\"", d, useragent, r, NULL \
+		"st -e /bin/sh -c \"curl -J -O --user-agent '$1'" \
+		" --referer '$2'" \
+		" -b ~/.surf/cookies.txt -c ~/.surf/cookies.txt '$0';" \
+		" sleep 5;\"", \
+		d, useragent, r, NULL \
 	} \
 }
 
