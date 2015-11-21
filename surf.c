bc46e954f2b1298fc6312e6a4bc8ed63b3051ac3
Quentin Rameau <quinq@fifth.space>
2015-11-21 19:22:11 +0100

    Style change in includes order

--- surf.c
+++ surf.c
@@ -2,30 +2,30 @@
  *
  * To understand surf, start reading main().
  */
-#include <signal.h>
-#include <X11/X.h>
-#include <X11/Xatom.h>
-#include <gtk/gtkx.h>
-#include <gtk/gtk.h>
-#include <gdk/gdkx.h>
-#include <gdk/gdk.h>
-#include <gdk/gdkkeysyms.h>
-#include <string.h>
+#include <sys/file.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <webkit2/webkit2.h>
-#include <glib/gstdio.h>
-#include <JavaScriptCore/JavaScript.h>
-#include <sys/file.h>
 #include <libgen.h>
-#include <stdarg.h>
-#include <regex.h>
+#include <limits.h>
 #include <pwd.h>
+#include <regex.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <gdk/gdk.h>
+#include <gdk/gdkkeysyms.h>
+#include <gdk/gdkx.h>
+#include <glib/gstdio.h>
+#include <gtk/gtk.h>
+#include <gtk/gtkx.h>
+#include <JavaScriptCore/JavaScript.h>
+#include <webkit2/webkit2.h>
+#include <X11/X.h>
+#include <X11/Xatom.h>
 
 #include "arg.h"
 
