b8b0c050354d7961edeec74e644f09a3e2887074
Quentin Rameau <quinq@fifth.space>
2017-05-08 10:20:28 +0200

    Embed dmenu into the surf window by default

--- config.def.h
+++ config.def.h
@@ -55,7 +55,7 @@ static WebKitFindOptions findopts = WEBKIT_FIND_OPTIONS_CASE_INSENSITIVE |
         .v = (const char *[]){ "/bin/sh", "-c", \
              "prop=\"$(printf '%b' \"$(xprop -id $1 $2 " \
              "| sed \"s/^$2(STRING) = //;s/^\\\"\\(.*\\)\\\"$/\\1/\")\" " \
-             "| dmenu)\" && xprop -id $1 -f $3 8s -set $3 \"$prop\"", \
+             "| dmenu -w $1)\" && xprop -id $1 -f $3 8s -set $3 \"$prop\"", \
              "surf-setprop", winid, r, s, NULL \
         } \
 }
